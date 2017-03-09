c7f0e77b5cc4189542522db04eac67b005baa786
Arosha Bandara <arosha.bandara@open.ac.uk>
2017-03-09 23:55:56 +0000

    Completed first basic app, with Intent and two activities

--- app/src/main/java/uk/ac/open/akb235/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/uk/ac/open/akb235/myfirstapp/DisplayMessageActivity.java
@@ -11,12 +11,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
